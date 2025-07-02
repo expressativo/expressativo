595a4d674ba14c55852c7187add22426af98d11f
Javier A Mora gil <j4viermora@gmail.com>
2025-07-02 02:32:45 +0200

    feat: basic profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,9 @@
 class ProfilesController < ApplicationController
   def index
-    @user  = current_user
+    @user = current_user
   end
   def show
-    @user  = current_user
+    @user = current_user
   end
 
   def edit
@@ -11,5 +11,17 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @user  = current_user
+    if @user.update(user_params)
+      redirect_to profiles_path, notice: "Perfil actualizado correctamente."
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email_address, :password, :password_confirmation, :first_name, :last_name)
   end
 end
