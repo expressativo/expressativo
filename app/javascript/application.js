4c4ef7909c93f98424cb0e19ad2bd414c39ec059
Javier A Mora gil <j4viermora@gmail.com>
2025-07-26 03:30:29 -0300

    feat: many stuffs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,37 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("mobile-menu-button");
-  const menu = document.getElementById("mobile-menu");
-  const iconOpen = button.querySelector("svg.block");
-  const iconClose = button.querySelector("svg.hidden");
-
-  button.addEventListener("click", function () {
-    const isOpen = menu.classList.contains("block");
-
-    // Toggle menu visibility
-    menu.classList.toggle("hidden");
-    menu.classList.toggle("block");
-
-    // Toggle icons
-    iconOpen.classList.toggle("hidden");
-    iconOpen.classList.toggle("block");
-    iconClose.classList.toggle("hidden");
-    iconClose.classList.toggle("block");
-
-    // Optionally update aria-expanded
-    button.setAttribute("aria-expanded", !isOpen);
-  });
-
-  // Inicialmente ocultamos el menú en caso de que esté visible
-  if (!menu.classList.contains("hidden")) {
-    menu.classList.add("hidden");
-  }
-});
-
 import "trix"
 import "@rails/actiontext"
