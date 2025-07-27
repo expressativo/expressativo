6fc44fba70a91e87e187e2e8c9b129e1ff0a44b3
Javier A Mora gil <j4viermora@gmail.com>
2025-07-27 19:03:53 -0300

    feat: mobile stuffs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,41 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+// En tu application.js
+document.addEventListener("trix-initialize", function(event) {
+  const toolbarElement = event.target.toolbarElement;
+  
+  // Buscar el botón de heading
+  const headingButton = toolbarElement.querySelector("[data-trix-attribute='heading1']");
+  
+  if (headingButton) {
+    // Crear dropdown con múltiples headings
+    const dropdown = document.createElement("select");
+    dropdown.innerHTML = `
+      <option value="">Normal</option>
+      <option value="heading1">H1</option>
+      <option value="heading2">H2</option>
+      <option value="heading3">H3</option>
+      <option value="heading4">H4</option>
+    `;
+    
+    dropdown.addEventListener("change", function() {
+        console.log(this.value)
+
+      if (this.value) {
+        console.log(this.value)
+        event.target.editor.activateAttribute(this.value);
+      } else {
+        console.log(this.value,"inside")
+
+        event.target.editor.deactivateAttribute("heading1");
+        event.target.editor.deactivateAttribute("heading2");
+        event.target.editor.deactivateAttribute("heading3");
+        event.target.editor.deactivateAttribute("heading4");
+      }
+    });
+    
+    headingButton.parentNode.replaceChild(dropdown, headingButton);
+  }
+});
