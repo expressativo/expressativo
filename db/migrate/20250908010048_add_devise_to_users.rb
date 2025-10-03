07e16666561e1b8bee8859871b79840792cc33d2
Javier A Mora gil <j4viermora@gmail.com>
2025-10-03 00:42:01 -0300

    feat: changes in migration

--- db/migrate/20250908010048_add_devise_to_users.rb
+++ db/migrate/20250908010048_add_devise_to_users.rb
@@ -4,15 +4,15 @@ class AddDeviseToUsers < ActiveRecord::Migration[8.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "" unless column_exists?(:users, :email)
+      t.string :encrypted_password, null: false, default: "" unless column_exists?(:users, :encrypted_password)
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      t.string   :reset_password_token unless column_exists?(:users, :reset_password_token)
+      t.datetime :reset_password_sent_at unless column_exists?(:users, :reset_password_sent_at)
 
       ## Rememberable
-      t.datetime :remember_created_at
+      t.datetime :remember_created_at unless column_exists?(:users, :remember_created_at)
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -37,8 +37,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[8.0]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email, unique: true unless index_exists?(:users, :email)
+    add_index :users, :reset_password_token, unique: true unless index_exists?(:users, :reset_password_token)
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
