6457c9398a76028e96653b14f37c67ca6a7701d3
Javier A Mora gil <j4viermora@gmail.com>
2026-01-08 11:16:12 -0300

    feat: redirect when user is auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,11 @@ class HomeController < ApplicationController
 
   def index
     # Landing page for Tivo
+    redirect_to_dashboard if user_signed_in?
   end
+
+  private
+    def redirect_to_dashboard
+      redirect_to projects_path
+    end
 end
