ccb2c1c4a24e3725edeb82c1968cc7238bd45db2
Javier A Mora gil <j4viermora@gmail.com>
2026-04-16 12:38:31 -0300

    feat: update action

--- app/controllers/projects/settings_controller.rb
+++ app/controllers/projects/settings_controller.rb
@@ -5,11 +5,19 @@ module Projects
     def index
     end
 
+    def update
+      @project.update(project_params)
+      redirect_to project_settings_path(@project)
+    end
+
     private
+
+    def project_params
+      params.require(:project).permit(:has_calendar)
+    end
 
     def set_project
       @project = Project.find(params[:project_id])
     end
   end
 end
-
