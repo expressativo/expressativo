1f0ee33a339d3870544b072c5f3bb541343df8eb
Javier Mora <j4viermora@gmail.com>
2026-05-19 21:40:35 -0300

    Fix NoMethodError on Rake.application in solid_queue and storage

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,5 +1,5 @@
 # Skip database configuration during asset precompilation
-unless defined?(Rake) && Rake.application.top_level_tasks.include?('assets:precompile')
+unless defined?(Rake) && Rake.respond_to?(:application) && Rake.application.top_level_tasks.include?('assets:precompile')
   Rails.application.configure do
     config.solid_queue.connects_to = { database: { writing: :queue, reading: :queue } }
   end
